ea213d99d052a0163970ce75be740d0cd38ed7d3
b3n0ni <benonipurty88@gmail.com>
2023-12-17 18:40:36 +0530

    Patch: UI fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :loans
-  before_create :set_default_wallet_amount
-  attr_accessor :name
+  before_create :set_default_user
 
   def admin?
     role == 'admin'
@@ -11,7 +10,9 @@ class User < ApplicationRecord
 
   private
 
-  def set_default_wallet_amount
+  def set_default_user
     self.wallet_amount = 10000
+    self.role = "user"
+    self.name = self.email.split('@').first
   end
 end
